5d6c63c1a69ce70d554f7cca9f36b86398037ed8
Alejandro1267 <164577387+Alejandro1267@users.noreply.github.com>
2026-02-11 15:48:56 -0700

    Add state to Form.tsx

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,17 +1,49 @@
+import {
+  useState,
+  type ChangeEvent,
+} from "react";
 import { countries } from "../../data/countries";
 import styles from "./Form.module.css";
+import type { Search } from "../../types";
 
 export default function Form() {
+  const [search, setSearch] = useState<Search>({
+    city: "",
+    country: "",
+  });
+  const [alert, setAlert] = useState("");
+
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>,
+  ) => {
+    setSearch({
+      ...search,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={handleSubmit}>
       <div className={styles.field}>
         <label htmlFor="city">Ciudad:</label>
-        <input id="city" type="text" name="city" placeholder="Ciudad" />
+        <input
+          id="city"
+          name="city"
+          type="text"
+          placeholder="Ciudad"
+          value={search.city}
+          onChange={handleChange}
+        />
       </div>
 
       <div className={styles.field}>
         <label htmlFor="country">País:</label>
-        <select>
+        <select
+          id="country"
+          name="country"
+          value={search.country}
+          onChange={handleChange}
+        >
           <option value="">--Selecciona un País--</option>
           {countries.map((country) => (
             <option key={country.code} value={country.code}>
